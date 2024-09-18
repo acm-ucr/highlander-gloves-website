e042a5e9bea4f848464b70b4684e6d9d9254a3c9
SafeDuck <schopra579@gmail.com>
2024-09-17 22:03:14 -0700

    fonts and colors

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,5 @@
+const Navigation = () => {
+  return <div className="bg-red-200">Navigation</div>;
+};
+
+export default Navigation;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,25 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/data/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "archivo-narrow": ["var(--font-archivo-narrow)"],
+        "archivo-black": ["var(--font-archivo-black)"],
+        "anek-telegu": ["var(--font-anek-telegu)"],
+      },
+      colors: {
+        hlg: {
+          "light-gray": "#D9D9D9",
+          "dark-gray": "#161616",
+          "red-100": "#D42C3C",
+          "red-200": "#882424",
+          "red-300": "#441212",
+        },
+      },
+    },
   },
   plugins: [],
 };
