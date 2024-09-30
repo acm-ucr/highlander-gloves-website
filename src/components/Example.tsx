4f70e2eb749c0c5383574208ecb33218b4e381d2
rzhou052 <rzhou052@ucr.edu>
2024-09-30 16:57:58 -0700

    Changed props

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -3,7 +3,7 @@ const Example = (props: { txt1: string; txt2: string; txt3: string }) => {
     <div className="flex w-5/6 flex-col gap-3 bg-blue-200 text-3xl">
       <div className="w-1/3 bg-red-200">{props.txt1}</div>
       <div className="w-1/3 bg-red-200">{props.txt2}</div>
-      <div className="w-1/3 bg-red-200">Add prop 3 here</div>
+      <div className="w-1/3 bg-red-200">{props.txt3}</div>
     </div>
   );
 };
