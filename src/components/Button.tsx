eae5b4104dfb10ea64606c00f1b12904810d4851
aldriebarcena <aldrievbarcena@gmail.com>
2024-10-04 18:28:19 -0700

    Create button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = (props: { txt: string; link: string }) => {
+  return (
+    <div className="box-border flex h-[82px] w-[392px] items-center justify-center rounded-[50px] border-[5px] border-hlg-red-300 bg-hlg-red-200 font-archivo-black text-[36px]">
+      <div className="relative inline-block">
+        <span className="absolute left-2 top-1.5 text-black opacity-100">
+          {props.txt}
+        </span>
+        <a href={props.link} className="relative text-white">
+          {props.txt}
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Button;
