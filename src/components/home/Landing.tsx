04ffdc0b6fa7bfe97ef80a4d3b9b33d8efdeb077
felix diaz <felixmdm321@gmail.com>
2024-10-05 01:06:38 -0700

    landing page banner

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+import banner from "@/public/home/landing.webp";
+
 const Landing = () => {
   return (
-    <div className="flex w-full items-center justify-center text-3xl">
-      {" "}
-      Landing
+    <div className="relative mt-[5.8%] flex flex-row items-center justify-center text-[115px]">
+      <div className="absolute inset-0 flex flex-col items-center font-archivo-black">
+        <p className="relative right-[36.2%] top-[4%]">UCR</p>
+        <p className="relative bottom-[6%]">HIGHLANDER</p>
+        <p className="relative bottom-[15%] left-[28%]">GLOVES</p>
+      </div>
+      <Image className="w-screen" src={banner} alt="Landing" />
     </div>
   );
 };
