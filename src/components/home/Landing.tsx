1725375e488ef8dad5decc44e8714b7dac2763c0
felix diaz <felixmdm321@gmail.com>
2024-10-06 11:30:19 -0700

    tailwind text size classes

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -3,11 +3,11 @@ import banner from "@/public/home/landing.webp";
 
 const Landing = () => {
   return (
-    <div className="relative mt-[5.8%] flex flex-row items-center justify-center text-[115px]">
+    <div className="relative mt-[5.8%] flex flex-row items-center justify-center text-8xl">
       <div className="absolute inset-0 flex flex-col items-center font-archivo-black">
-        <p className="relative right-[36.2%] top-[4%]">UCR</p>
-        <p className="relative bottom-[6%]">HIGHLANDER</p>
-        <p className="relative bottom-[15%] left-[28%]">GLOVES</p>
+        <p className="relative right-[33%] top-[12.5%]">UCR</p>
+        <p className="relative top-[15.5%]">HIGHLANDER</p>
+        <p className="relative left-[27%] top-[20%]">GLOVES</p>
       </div>
       <Image className="w-screen" src={banner} alt="Landing" />
     </div>
