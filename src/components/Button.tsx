f95d333816515e7a8a4b2609ada935a19a5de849
aldriebarcena <aldrievbarcena@gmail.com>
2024-10-06 14:57:31 -0700

    Remove hardcoded values in button, install react-icons to remove error

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const Button = (props: { txt: string; link: string }) => {
   return (
-    <div className="box-border flex h-[82px] w-[392px] items-center justify-center rounded-[50px] border-[5px] border-hlg-red-300 bg-hlg-red-200 font-archivo-black text-[36px]">
+    <div className="box-border flex w-1/6 items-center justify-center rounded-full border-[5px] border-hlg-red-300 bg-hlg-red-200 py-2 font-archivo-black text-3xl">
       <div className="relative inline-block">
         <span className="absolute left-2 top-1.5 text-black opacity-100">
           {props.txt}
         </span>
-        <a href={props.link} className="relative text-white">
+        <Link href={props.link} className="relative text-white">
           {props.txt}
-        </a>
+        </Link>
       </div>
     </div>
   );
