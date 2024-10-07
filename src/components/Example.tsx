b956e4e418b883e73fa4e37dad38355cf5c7bd9d
Raymond Guo <raymondguo@Raymonds-MacBook-Pro.local>
2024-10-07 12:50:16 -0700

    fixed the example component

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,9 +1,9 @@
 const Example = (props: { txt1: string; txt2: string; txt3: string }) => {
   return (
-    <div className="flex w-5/6 flex-col gap-3 bg-blue-200 text-3xl">
-      <div className="w-1/3 bg-red-200">{props.txt1}</div>
-      <div className="w-1/3 bg-red-200">{props.txt2}</div>
-      <div className="w-1/3 bg-red-200">Add prop 3 here</div>
+    <div className="flex w-5/6 flex-row items-stretch gap-3 bg-blue-200 p-8 text-3xl">
+      <div className="w-1/3 bg-red-200 text-center">{props.txt1}</div>
+      <div className="w-1/3 bg-red-200 text-center">{props.txt2}</div>
+      <div className="w-1/3 bg-red-200 text-center">{props.txt3}</div>
     </div>
   );
 };
