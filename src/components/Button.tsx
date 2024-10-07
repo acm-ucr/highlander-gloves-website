c6e824bc22a4ac45021ca7efd41668a42e7c5372
aldriebarcena <aldrievbarcena@gmail.com>
2024-10-06 18:35:11 -0700

    Format with prettier and eslint

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,16 @@ import Link from "next/link";
 
 const Button = (props: { txt: string; link: string }) => {
   return (
-    <Link href={props.link} className="box-border flex w-1/6 items-center justify-center rounded-full border-[5px] border-hlg-red-300 bg-hlg-red-200 py-2 font-archivo-black text-3xl">
-        <div className="relative inline-block">
-          <span className="absolute left-2 top-1.5 text-black opacity-100">
-            {props.txt}
-          </span>
-          <span className="relative text-white">{props.txt}</span>
-        </div>
+    <Link
+      href={props.link}
+      className="box-border flex w-1/6 items-center justify-center rounded-full border-[5px] border-hlg-red-300 bg-hlg-red-200 py-2 font-archivo-black text-3xl"
+    >
+      <div className="relative inline-block">
+        <span className="absolute left-2 top-1.5 text-black opacity-100">
+          {props.txt}
+        </span>
+        <span className="relative text-white">{props.txt}</span>
+      </div>
     </Link>
   );
 };
