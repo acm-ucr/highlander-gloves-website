ec76c5b74ab42b6096371ceb19c5cd0a44e89c46
Kaylie Zhao <zhaokaylie@gmail.com>
2024-10-06 22:01:27 -0700

    fixed font, deleted js file

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,20 +1,5 @@
-import React from "react";
-
-const SubHeader = (props: { position: string; title: string }) => {
-  return (
-    <div
-      className={`w-1/8 flex flex-row border-b-4 border-t-4 border-white p-2 pl-[5%] pr-[5%] text-3xl text-white ${props.position === "left" ? "border-r-4" : "border-l-4"}`}
-    >
-      {props.title}
-    </div>
-  );
+const SubHeader = () => {
+  return <div>SubHeader</div>;
 };
 
 export default SubHeader;
-
-
-// const SubHeader = () => {
-//   return <div>SubHeader</div>;
-// };
-
-// export default SubHeader;
--- src/components/home/GetStarted.tsx
+++ src/components/home/GetStarted.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import SubHeader from "@/components/SubHeader";
+import Header from "@/components/Header";
 
 const Example = (props: { txt1: string }) => {
   return (
     <div className="flex w-full flex-col gap-3 text-3xl">
       {/* change to justify-end when positioning to right */}
       <div className="flex w-full justify-start">
-        <SubHeader position="left" title={props.txt1} />
+        <Header position="left" title={props.txt1} />
       </div>
     </div>
   );
