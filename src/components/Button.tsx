c57bba081dadc564dd3803adbe09689cd30a0de1
aldriebarcena <aldrievbarcena@gmail.com>
2024-10-06 18:28:52 -0700

    Make Link tag outer container

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,16 +3,14 @@ import Link from "next/link";
 
 const Button = (props: { txt: string; link: string }) => {
   return (
-    <div className="box-border flex w-1/6 items-center justify-center rounded-full border-[5px] border-hlg-red-300 bg-hlg-red-200 py-2 font-archivo-black text-3xl">
-      <div className="relative inline-block">
-        <span className="absolute left-2 top-1.5 text-black opacity-100">
-          {props.txt}
-        </span>
-        <Link href={props.link} className="relative text-white">
-          {props.txt}
-        </Link>
-      </div>
-    </div>
+    <Link href={props.link} className="box-border flex w-1/6 items-center justify-center rounded-full border-[5px] border-hlg-red-300 bg-hlg-red-200 py-2 font-archivo-black text-3xl">
+        <div className="relative inline-block">
+          <span className="absolute left-2 top-1.5 text-black opacity-100">
+            {props.txt}
+          </span>
+          <span className="relative text-white">{props.txt}</span>
+        </div>
+    </Link>
   );
 };
 
