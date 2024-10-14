21a83350a64ce28970ad9516cd1a08afbc25729c
Justin P <justinp@ucr-secure-42-10-13-217-160.wnet.ucr.edu>
2024-10-14 01:34:04 -0700

    added FAQ

--- src/components/about/FAQ.tsx
+++ src/components/about/FAQ.tsx
@@ -1,6 +1,6 @@
 const FAQ = (props: { text: string }) => {
   return (
-    <div className="align-center m-8 flex aspect-[4/3] w-1/4 flex-col justify-center bg-hlg-red-200 p-8 text-center font-anek-telegu text-xl font-semibold leading-tight text-white shadow-[22px_22px_0px_0px_#441212]">
+    <div className="align-center m-8 flex aspect-[4/3] w-1/5 flex-col justify-center bg-hlg-red-200 p-8 text-center font-anek-telegu text-xl font-semibold leading-tight text-white shadow-[22px_22px_0px_0px_#441212]">
       {props.text}
     </div>
   );
