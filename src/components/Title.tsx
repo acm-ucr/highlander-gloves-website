7e672a4dc8f046dab71e62cfc31a45a237d04812
aldriebarcena <aldrievbarcena@gmail.com>
2024-10-16 15:30:24 -0700

    Remove conditional for upcoming events image, add static import

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
-const Title = (props: { txt: string; img: string; alt: string }) => {
-  const imgStyle =
-    props.img === "/title/meetTheBoard.jpeg"
-      ? {}
-      : { objectPosition: "center 20%" };
+type photoProps = {
+  image: StaticImport;
+  width: string;
+  alt: string;
+  txt: string;
+};
 
+const Title = ({ image, alt, txt }: photoProps) => {
   return (
     <div className="relative h-[600px] w-full">
-      <Image
-        src={props.img}
-        alt={props.alt}
-        fill={true}
-        style={{ ...imgStyle }}
-        className="object-cover brightness-[30%]"
-      />
+      <Image src={image} alt={alt} fill={true} className="object-cover" />
 
       <div className="absolute inset-0 flex items-center justify-center font-archivo-black">
         <div className="max-w-[800px] text-center">
           <span className="text-9xl font-bold text-white drop-shadow-[20px_15px_0_#000000]">
-            {props.txt}
+            {txt}
           </span>
         </div>
       </div>
