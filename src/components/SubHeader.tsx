b8d32ab7751ced60c5d4ffa5ac72af2122a6ecc8
Raymond Guo <raymondguo@Raymonds-MacBook-Pro.local>
2024-10-17 01:27:10 -0700

    left it as a component

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,10 +1,10 @@
 const SubHeader = (props: { txt: string }) => {
   return (
     <div className="flex w-full flex-col items-center gap-3">
-      <div className="text-white font-archivo-black text-3xl tracking-wider">
+      <div className="font-archivo-black text-3xl tracking-wider text-white">
         {props.txt}
       </div>
-      <div className="w-1/3 h-1.5 bg-white" />
+      <div className="h-1.5 w-1/3 bg-white" />
     </div>
   );
 };
