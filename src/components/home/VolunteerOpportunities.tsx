0fae60b9d790574afa0e158ea0a9f54d8a0ff5d1
Shirley Duong <shirleytduong@gmail.com>
2024-10-16 20:42:58 -0700

    update padding and self-closing tag

--- src/components/home/VolunteerOpportunities.tsx
+++ src/components/home/VolunteerOpportunities.tsx
@@ -6,7 +6,7 @@ const VolunteerOpportunities = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center pt-10 text-3xl">
       <p className="pb-3 font-archivo-black">Volunteer Opportunities</p>
-      <div className="flex flex-col border-t-[3px] border-white pl-[12%] pr-[12%]"></div>
+      <div className="flex flex-col border-t-[3px] border-white w-3/12" />
       <p className="w-5/6 p-12 text-center font-anek-telegu">
         Highlander Gloves is partnered with Riverside PD to help tutor and coach
         at-risk kids around the area. As a volunteer, you'll be helping clean
@@ -14,7 +14,7 @@ const VolunteerOpportunities = () => {
         have little to no experience, we'll show you the ropes, so please feel
         free to show up! We'd love all the help we can get!
       </p>
-      <div className="flex w-full flex-row pb-10 pt-10">
+      <div className="flex w-full flex-row py-10">
         <div className="flex items-center justify-center">
           <Photo
             image={VolunteerPhoto}
