0f48545d29a68e1f0e54cc77db4f53b39ee6918d
Richard Zhou <rzhou052@ucr.edu>
2024-10-20 21:02:14 -0700

    Added board photos to public + small edit to spacing in Boards

--- src/components/board/Boards.tsx
+++ src/components/board/Boards.tsx
@@ -1,12 +1,17 @@
 import Board from "./Board";
 import BoardMembers from "@/data/BoardMembers.json";
-import example from "@/public/example.webp";
+import Example from "@/public/example.webp";
 
 const Boards = () => {
   return (
-    <div className="flex w-full flex-row justify-center text-3xl">
+    <div className="flex flex-row flex-wrap justify-center gap-x-[128px] gap-y-[176px] pb-[300px] pt-[150px]">
       {BoardMembers.map((Member) => (
-        <Board photo={example} name={Member.name} position={Member.position} />
+        <Board
+          key={Member.key}
+          photo={Example}
+          name={Member.name}
+          position={Member.position}
+        />
       ))}
     </div>
   );
