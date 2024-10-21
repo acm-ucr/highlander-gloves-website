afc2d9caa2c6abdcc01a9446630fd5d1eb9ec66a
aldriebarcena <aldrievbarcena@gmail.com>
2024-10-21 16:52:22 -0700

    Add header, create initial card

--- src/components/about/FAQs.tsx
+++ src/components/about/FAQs.tsx
@@ -1,5 +1,19 @@
+import React from 'react'
+import Header from "@/components/Header";
+
 const FAQs = () => {
-  return <div>FAQs</div>;
+  return (
+    <div className="w-full relative">
+        <div className="flex text-xl justify-start">
+          <div className=""></div>
+          <Header position="left" title="FAQs" />
+        </div>
+
+        <div className='bg-hlg-red-200 w-2/5 h-96 shadow-[22px_30px_0px_0px_#441212] flex justify-center items-center text-4xl font-archivo-black text-center'>
+          When and where is practice?</div>
+        
+    </div>
+  );
 };
 
 export default FAQs;
