786167a3a2afd79f6b8cd917d81aa9ad69d26269
Richard Zhou <rzhou052@ucr.edu>
2024-10-20 18:36:57 -0700

    Basic Boards component functionality

--- src/components/board/Boards.tsx
+++ src/components/board/Boards.tsx
@@ -1,6 +1,14 @@
+import Board from "./Board";
+import BoardMembers from "@/data/BoardMembers.json";
+import example from "@/public/example.webp";
+
 const Boards = () => {
   return (
-    <div className="flex w-full flex-row justify-center text-3xl">Boards</div>
+    <div className="flex w-full flex-row justify-center text-3xl">
+      {BoardMembers.map((Member) => (
+        <Board photo={example} name={Member.name} position={Member.position} />
+      ))}
+    </div>
   );
 };
 
