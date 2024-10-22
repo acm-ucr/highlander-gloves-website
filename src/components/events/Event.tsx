19d904dc91f44acdb03bcc0bc1d7f872fafce70a
kevin razo <recklezzninja11@gmail.com>
2024-10-21 20:02:37 -0700

    Built Example Component

--- src/components/events/Event.tsx
+++ src/components/events/Event.tsx
@@ -1,5 +1,33 @@
-const Event = () => {
-  return <div>Event</div>;
+import Header from "@/components/Header";
+
+type EventProps = {
+  name: string;
+  description: string;
+  location: string;
+  time: string;
+  side: "left" | "right";
+};
+
+
+const Event = ({ name, description, location, time, side }: EventProps) => {
+  return (
+    <div
+      className={`mt-8 mb-8 flex max-w-xl flex-col text-white ${
+        side === "left" ? "mr-auto" : "ml-auto"
+      }`}
+    >
+
+      <div className="flex-row justify-center">
+        <Header position={side} title={name} />
+      </div>
+        
+      <div className= "mt-4 flex flex-col w-full justify-center p-6 text-2xl" >
+        <p>{description}</p>
+        <p>{location}</p>
+        <p>{time}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Event;
