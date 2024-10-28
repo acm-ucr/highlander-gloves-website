413ffe8d50ad72b6e0b7a1ceae99ecc773768035
unknown <fongivann@gmail.com>
2024-10-28 00:17:12 -0700

    Responsify Landing Component

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -3,11 +3,17 @@ import banner from "@/public/home/landing.webp";
 
 const Landing = () => {
   return (
-    <div className="relative flex flex-row items-center justify-center text-8xl">
+    <div className="relative flex flex-row items-center justify-center text-3xl md:text-5xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
       <div className="absolute inset-0 flex flex-col items-center font-archivo-black">
-        <p className="relative right-[33%] top-[12.5%]">UCR</p>
-        <p className="relative top-[15.5%]">HIGHLANDER</p>
-        <p className="relative left-[27%] top-[20%]">GLOVES</p>
+        <p className="xl:top[20%] relative right-[35%] top-[8%] md:top-[15%] lg:top-[17%]">
+          UCR
+        </p>
+        <p className="xl:top[20%] relative right-[8%] top-[8%] md:right-[6%] md:top-[15%] lg:top-[17%]">
+          HIGHLANDER
+        </p>
+        <p className="xl:top[20%] relative left-[26%] top-[8%] md:top-[15%] lg:top-[17%]">
+          GLOVES
+        </p>
       </div>
       <Image className="w-screen" src={banner} alt="Landing" />
     </div>
