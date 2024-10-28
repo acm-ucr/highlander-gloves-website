17ad1d0e054bdefcc12eebde0def88f01f029079
Kaylie Zhao <zhaokaylie@gmail.com>
2024-10-28 00:45:17 -0700

    responsify faqs

--- src/components/about/FAQs.tsx
+++ src/components/about/FAQs.tsx
@@ -6,11 +6,16 @@ import faqs from "@/data/Faq";
 const FAQs = () => {
   return (
     <div className="relative w-full">
-      <div className="flex justify-start text-xl">
+      <div className="hidden justify-center md:flex md:justify-start md:text-xl">
         <Header position="left" title="FAQs" />
       </div>
+      <div className="flex justify-center md:hidden">
+        <div className="border-b-4 px-[30%] py-4 font-archivo-black text-2xl">
+          FAQS
+        </div>
+      </div>
 
-      <div className="m-16 grid grid-cols-2">
+      <div className="m-[10%] grid md:m-16 md:grid-cols-2">
         {faqs.map((faq, index) => (
           <FAQ key={index} text={faq.question} />
         ))}
