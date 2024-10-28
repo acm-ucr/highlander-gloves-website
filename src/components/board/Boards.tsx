18974324e014ed481252b5d3dc9677a3cd884e80
Justin <jp89213@gmail.com>
2024-10-28 00:08:52 -0700

    responsified the iphone SE and laptop version

--- src/components/board/Boards.tsx
+++ src/components/board/Boards.tsx
@@ -1,6 +1,21 @@
+import React from "react";
+import Board from "./Board";
+import { Members } from "@/data/BoardMembers";
+
 const Boards = () => {
   return (
-    <div className="flex w-full flex-row justify-center text-3xl">Boards</div>
+    <div className="grid grid-cols-3 gap-[10%] p-[10%] pb-[50%] pt-[8%]">
+      {Members.map((Member) => (
+        <div className="max-w-[348px]">
+          <Board
+            key={Member.key}
+            photo={Member.image}
+            name={Member.name}
+            position={Member.position}
+          />
+        </div>
+      ))}
+    </div>
   );
 };
 
--- src/components/home/Times.tsx
+++ src/components/home/Times.tsx
@@ -2,17 +2,20 @@ import Header from "@/components/Header";
 
 const Times = () => {
   return (
-    <div className="flex w-full flex-col justify-center gap-12">
-      <div className="flex justify-end">
+    <div className="flex w-full flex-col items-center justify-center gap-6 sm:items-end sm:gap-12">
+      <div className="flex w-full justify-center md:hidden">
+        <Header position="middle" title="&emsp; TIMES &emsp;" />
+      </div>
+      <div className="hidden w-full justify-end md:flex">
         <Header position="right" title="&emsp; TIMES &emsp;" />
       </div>
-      <div className="flex w-full flex-col justify-end pr-11 font-anek-telegu">
-        <p className="pb-6 text-right">
+      <div className="flex w-11/12 flex-col justify-end font-anek-telegu sm:w-full sm:pr-11">
+        <p className="pb-6 text-center text-lg sm:text-right sm:text-3xl">
           Tuesdays: 4:00 pm - 6:00 pm <br />
           Wednesdays: 4:00 pm - 6:00 pm <br />
           Thursdays: 4:30 pm - 6:30 pm <br />
         </p>
-        <p className="text-right text-2xl">
+        <p className="text-center text-sm sm:text-right sm:text-2xl">
           (P.S. Thursdays will consist of only boxing, so more experienced
           members, this is your time to shine!)
         </p>
