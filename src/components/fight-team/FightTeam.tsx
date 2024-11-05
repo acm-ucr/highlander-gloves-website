8ce5bf82657441396ce7f486808581179696b403
Justin <jp89213@gmail.com>
2024-11-04 16:15:39 -0800

    Responsified Fight Team

--- src/components/fight-team/FightTeam.tsx
+++ src/components/fight-team/FightTeam.tsx
@@ -5,16 +5,22 @@ import coach from "@/public/fight-team/coach.webp";
 
 const FightTeam = () => {
   return (
-    <div className="flex flex-col justify-center md:gap-10 md:text-2xl">
-      <div className="flex justify-center text-xl md:justify-start md:text-2xl">
+    <div className="mt-5 flex flex-col md:mt-10 md:gap-10 md:text-2xl">
+      <div className="flex justify-center text-base md:justify-start md:text-2xl">
         <div className="border-l-4 md:border-l-0" />
         <Header position="left" title="WHAT IS FIGHT TEAM?" />
       </div>
       <div className="flex text-center md:text-left md:text-2xl">
         <div className="flex w-full flex-col md:flex-row">
+          <div className="mt-4 flex w-full items-center justify-center justify-self-center md:hidden">
+            <Photo width={"w-1/3"} image={coach} alt="Coach Israel Salgado" />
+          </div>
+          <div className="mt-4 hidden w-full items-center justify-center justify-self-center md:order-last md:flex md:size-auto md:items-center">
+            <Photo width={"w-2/3"} image={coach} alt="Coach Israel Salgado" />
+          </div>
           <div className="flex flex-col items-center justify-center md:w-3/5">
             <div className="w-4/6">
-              <div className="font-anek-tekygu flex flex-col font-thin">
+              <div className="font-anek-tekygu mt-2 flex flex-col text-xs md:text-lg md:font-thin">
                 Fight team is UCR's competitive boxing team that is a branch of
                 highlander Gloves They have their own training and program
                 seperate from the general members of Highlander Gloves. Fight
@@ -25,16 +31,13 @@ const FightTeam = () => {
                 during practice. This team is led by Coach Israel and our
                 Womens' and Mens' Captains.
               </div>
-              <div className="flex w-4/5 flex-col font-archivo-narrow font-medium md:mt-8">
+              <div className="mt-5 flex w-full flex-col justify-items-center justify-self-center font-archivo-narrow text-xs font-medium md:mt-8 md:justify-self-start md:text-lg">
                 #2 Men's Team and #3 Women's Team USIBA '24
               </div>
-              <div className="flex w-2/3 flex-col font-archivo-narrow font-medium">
+              <div className="flex w-2/3 flex-col justify-self-center font-archivo-narrow text-xs font-medium md:justify-self-start md:text-lg">
                 #1 Women's Team USIBA '23
               </div>
             </div>
-          </div>
-          <div className="flex justify-end md:items-center">
-            <Photo width={"w-2/3"} image={coach} alt="Coach Israel Salgado" />
           </div>
         </div>
       </div>
