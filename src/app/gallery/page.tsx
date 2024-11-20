cbef513784515a5e42c3b0443d6651e5eadbad58
aldriebarcena <aldrievbarcena@gmail.com>
2024-11-19 16:49:14 -0800

    Run format and eslint

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { Gallery } from "react-grid-gallery";
 import Lightbox from "react-image-lightbox";
@@ -51,14 +51,43 @@ type ImageProps = {
 };
 
 const importedImages = [
-  image1, image2, image3, image4, image5,
-  image6, image7, image8, image9, image10,
-  image11, image12, image13, image14, image15,
-  image16, image17, image18, image19, image20,
-  image21, image22, image23, image24, image25,
-  image26, image27, image28, image29, image30,
-  image31, image32, image33, image34, image35,
-  image36, image37,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+  image28,
+  image29,
+  image30,
+  image31,
+  image32,
+  image33,
+  image34,
+  image35,
+  image36,
+  image37,
 ];
 
 const images: ImageProps[] = importedImages.map((image, index) => ({
@@ -67,7 +96,6 @@ const images: ImageProps[] = importedImages.map((image, index) => ({
   height: image.height,
   alt: `Image ${index + 1}`,
 }));
-
 
 const GalleryComponent = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -80,21 +108,16 @@ const GalleryComponent = () => {
 
   return (
     <div className="w-full">
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col items-center justify-center">
         <p className="py-1 font-archivo-black">2023 - 2024</p>
-        <div className="flex flex-col border-t-[2px] border-white w-36 pb-3" />
+        <div className="flex w-36 flex-col border-t-[2px] border-white pb-3" />
       </div>
-      <Gallery
-        images={images}
-        onClick={(index) => handleImageClick(index)}
-      />
+      <Gallery images={images} onClick={(index) => handleImageClick(index)} />
       {isOpen && (
         <Lightbox
           mainSrc={images[photoIndex].src}
           nextSrc={images[(photoIndex + 1) % images.length].src}
-          prevSrc={
-            images[(photoIndex + images.length - 1) % images.length].src
-          }
+          prevSrc={images[(photoIndex + images.length - 1) % images.length].src}
           onCloseRequest={() => setIsOpen(false)}
           onMovePrevRequest={() =>
             setPhotoIndex((photoIndex + images.length - 1) % images.length)
@@ -107,6 +130,5 @@ const GalleryComponent = () => {
     </div>
   );
 };
-
 
 export default GalleryComponent;
