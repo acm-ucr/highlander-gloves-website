8366a10675fa8cba7f389ddd4d72224570ba6fe4
SafeDuck <schopra579@gmail.com>
2024-11-22 16:21:58 -0800

    Added Socials SVG mobile

--- src/components/home/Socials.tsx
+++ src/components/home/Socials.tsx
@@ -7,10 +7,22 @@ import {
   FaFacebookF,
 } from "react-icons/fa";
 import socialMedia from "@/public/home/socialMedia.svg";
+import socialMediaMobile from "@/public/home/socialMediaMobile.svg";
+
 const Socials = () => {
   return (
     <div className="relative mb-10 flex w-full flex-col items-center justify-center text-3xl">
-      <Image src={socialMedia} alt="Social Media Asset" className="w-full" />
+      <Image
+        src={socialMedia}
+        alt="Social Media Asset"
+        className="hidden w-full md:block"
+      />
+      <Image
+        src={socialMediaMobile}
+        alt="Social Media Asset"
+        className="w-full md:hidden"
+      />
+
       <FaDiscord className="absolute duration-200 hover:scale-[110%] hover:cursor-pointer xl:left-[13.5%] xl:top-[45%] xl:text-8xl 2xl:left-[14%] 2xl:top-[46%] 2xl:text-9xl" />
       <FaTiktok className="absolute duration-200 hover:scale-[110%] hover:cursor-pointer xl:bottom-[7%] xl:left-[31.5%] xl:text-8xl 2xl:bottom-[7%] 2xl:left-[32%] 2xl:text-9xl" />
       <FaInstagram className="absolute duration-200 hover:scale-[110%] hover:cursor-pointer xl:left-[48.5%] xl:top-[45%] xl:text-8xl 2xl:left-[49%] 2xl:top-[46%] 2xl:text-9xl" />
