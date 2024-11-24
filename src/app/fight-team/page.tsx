ca8431c064564fff72b605ded47c60b62e43aac8
Raymond Guo <raymondguo@raymonds-mbp.lan>
2024-11-23 22:24:46 -0800

    added title component to fight team page

--- src/app/fight-team/page.tsx
+++ src/app/fight-team/page.tsx
@@ -1,9 +1,12 @@
 import FightTeam from "@/components/fight-team/FightTeam";
 import Gallery from "@/components/fight-team/Gallery";
+import Title from "@/components/Title";
+import fightTeam from "@/public/title/fightTeam.webp";
 
 const page = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-10">
+      <Title image={fightTeam} alt="Fight Team Header Image" txt="FIGHT TEAM" />
       <FightTeam />
       <Gallery />
     </div>
