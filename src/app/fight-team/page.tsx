75111b84f75ae8839bab068e9a702c6d71b89a3d
SafeDuck <schopra579@gmail.com>
2024-11-29 22:30:00 -0800

    Volunteer Form Change

--- src/app/fight-team/page.tsx
+++ src/app/fight-team/page.tsx
@@ -1,14 +1,16 @@
 import FightTeam from "@/components/fight-team/FightTeam";
-import Gallery from "@/components/fight-team/Gallery";
+import GalleryComponent from "@/components/fight-team/Gallery";
 import Title from "@/components/Title";
 import fightTeam from "@/public/title/fightTeam.webp";
+import Header from "@/components/Header";
 
 const page = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-5">
+    <div className="flex w-full flex-col items-center justify-center gap-10">
       <Title webp={fightTeam} alt="Fight Team Header Image" txt="FIGHT TEAM" />
       <FightTeam />
-      <Gallery />
+      <Header title="GALLERY" position="middle" />
+      <GalleryComponent />
     </div>
   );
 };
