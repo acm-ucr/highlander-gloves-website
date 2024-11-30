7c905257bf3e83d32c4c2d4cf62763d842e12183
Mustafa Hassan <mustafahassan@ucr-secure-34-10-13-186-6.wnet.ucr.edu>
2024-11-30 15:21:16 -0800

    updated Board title component

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,10 +1,13 @@
 import Boards from "@/components/board/Boards";
 import Image from "next/image";
 import line from "@/public/board/line.svg";
+import meetTheBoard from "@/public/title/meetTheBoard.webp";
+import Title from "@/components/Title";
 
 const page = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center">
+      <Title webp={meetTheBoard} alt="Meet the Board Header Image" txt="MEET THE BOARD"/>
       <Boards />
 
       <Image
@@ -17,3 +20,5 @@ const page = () => {
 };
 
 export default page;
+
+
